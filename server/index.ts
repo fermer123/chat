ff4568903c1e8d33e3050c91baf4a281ece2099e
fermer123 <fermer_525@mail.ru>
2023-05-12 00:38:05 +0500

    er on local

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,14 @@
 import express, {Express, Request, Response} from 'express';
 export const port = 3000;
 const app: Express = express();
+const mockResponse = {
+  foo: 'bar',
+  bar: 'foo',
+};
 app.get('/', (req: Request, res: Response) => {
   res.send(`nodemone & concurently & port ${port}`);
 });
-
+app.get('/api', (req, res) => {
+  res.send(mockResponse);
+});
 export default app;
--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,4 @@
-import app, {port} from '.';
+import app, {port} from './index';
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
