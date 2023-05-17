386b28b440497e32c1f12288ffa2529012c286da
fermer123 <fermer_525@mail.ru>
2023-05-18 02:35:14 +0500

    auth with jwt & json in dist

--- server/routes.ts
+++ server/routes.ts
@@ -1,12 +1,22 @@
 import express, {Request, Response} from 'express';
-import {port} from './constants/constants';
+import {USER_JSON_FILE, port} from './constants/constants';
+import bodyParser from 'body-parser';
+import fs from 'fs';
 
 const router = express.Router();
+const jsonParser = bodyParser.json();
 
+const users = JSON.parse(fs.readFileSync(USER_JSON_FILE, 'utf8'));
 router.get('/', (req: Request, res: Response) => {
-  res.json(`nodemone & concurently & port ${port}`);
+  res.json(`nodemone & concurently & port ${port} ${JSON.stringify(users)}`);
 });
 
-router.get('/auth', (req, res) => {});
+router.post('/login', jsonParser, (req, res) => {
+  const {email, password} = req.body;
+});
+
+router.post('/register', jsonParser, (req, res) => {
+  const {email, password} = req.body;
+});
 
 export default router;
