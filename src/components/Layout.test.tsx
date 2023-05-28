09c977357dcaf8bb775383a4db4b3edd68fa9be5
fermer123 <fermer_525@mail.ru>
2023-05-29 00:16:35 +0500

    layout test and need to fix lazy with suspens

--- src/components/Layout.test.tsx
+++ src/components/Layout.test.tsx
@@ -1,5 +1,6 @@
 import renderer from 'react-test-renderer';
 import {MemoryRouter} from 'react-router-dom';
+import {render, screen} from '@testing-library/react';
 import Layout from './Layout';
 
 describe('Layout', () => {
@@ -12,5 +13,21 @@ describe('Layout', () => {
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
+  });
+  test('expect header when location is not "/"', () => {
+    render(
+      <MemoryRouter initialEntries={['/room']}>
+        <Layout />
+      </MemoryRouter>,
+    );
+    expect(screen.getByTestId('AvatarButton')).toBeInTheDocument();
+  });
+  test('expect header not to be in document when location is "/"', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Layout />
+      </MemoryRouter>,
+    );
+    expect(screen.queryByTestId('AvatarButton')).not.toBeInTheDocument();
   });
 });
