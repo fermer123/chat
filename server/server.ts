4db069c24fe88b57a0d4a5045647c7dec539b7e5
fermer123 <fermer_525@mail.ru>
2023-07-02 02:21:19 +0500

    server

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
 import {port} from './src/constants/constants';
 import server from './src/index';
 
-server.listen(port, () => {
+server.listen(3000, () => {
   console.log(`Example app listening on port ${port}`);
 });
--- server/src/constants/constants.ts
+++ server/src/constants/constants.ts
@@ -3,6 +3,6 @@ import path from 'path';
 // export const HTML_DIR = path.join(__dirname, '../../../public');
 // console.log(HTML_DIR);
 // export const HTML_FILE = path.join(HTML_DIR, 'index.html');
-export const USERS_JSON_FILE = path.join(__dirname, './src/users.json');
+export const USERS_JSON_FILE = path.join(__dirname, '../users.json');
 
 export const port = 3000;
